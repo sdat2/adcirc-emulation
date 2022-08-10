306dec77039fc4eadf8ad02eb4de8b19746db417
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-08-10 13:46:51 +0100

    ext: sanitize

--- src/preprocessing/labels.py
+++ src/preprocessing/labels.py
@@ -2,5 +2,19 @@
 
 
 def sanitize(input: str) -> str:
+    """
+    Sanitize the labels.
+
+    Args:
+        input (str): input string.
+
+    Returns:
+        str: output string.
+
+    Example::
+        >>> from src.preprocessing.labels import sanitize
+        >>> sanitize("storm_surge")
+        'Storm Surge'
+    """
     output = input.replace("_", " ")
     return output.title()
