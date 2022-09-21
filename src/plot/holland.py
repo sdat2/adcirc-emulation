fdff2574e583c5c0a283e9e7e99951b43c374be5
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-09-21 22:58:22 +0100

    holland

--- src/plot/holland.py
+++ src/plot/holland.py
@@ -23,24 +23,23 @@ def plot_example() -> None:
     # velocities = holland2010(radii, bs_coeff=0.003, x_coeff=0.5, rmax=5000, vmax=50)
     ds = landings_only(katrina()).isel(date_time=2)
 
-    velocities = holland2010(
-        radii, 0.5, 8, ds["usa_rmw"].values, ds["usa_wind"].values
-    )
+    velocities = holland2010(radii, 0.5, 8, ds["usa_rmw"].values, ds["usa_wind"].values)
     plt.plot(radii, velocities)
     plt.xlabel("Radius (m)")
     plt.ylabel("Velocity (m s$^{-1}$)")
     plt.gca().xaxis.set_major_formatter(mtick.FormatStrFormatter("%.0e"))
 
-    radii_kat = [np.mean(ds[var].values) for var in ["usa_r34", "usa_r50", "usa_r64"]]
+    radii_kat = np.array(
+        [np.mean(ds[var].values) for var in ["usa_r34", "usa_r50", "usa_r64"]]
+    )
     speeds_kat = knots_to_ms(np.array([34, 50, 64]))
 
     plt.scatter(ds["usa_rmw"].values, ds["usa_wind"].values)
     plt.scatter(radii_kat, speeds_kat, c="red")
 
-    popt, func = holland_fitter_usa(ds)
-    print(popt)
-
-    plt.plot(radii, func(radii))
+    # popt, func = holland_fitter_usa(ds)
+    # print(popt)
+    # plt.plot(radii, func(radii))
 
     plt.savefig(os.path.join(FIGURE_PATH, "holland_example.png"))
     if in_notebook():
