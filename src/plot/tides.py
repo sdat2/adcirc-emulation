899bb416b4009394cc9f5348a1c9e0fb2ce45742
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-11-26 13:54:28 +0000

    better tidal plots

--- src/plot/tides.py
+++ src/plot/tides.py
@@ -1,24 +1,30 @@
 """Tidal Comparison Plots."""
+import os
 import xarray as xr
 import datetime
 import matplotlib.pyplot as plt
 from sithom.plot import plot_defaults
 from src.data_loading.tides import filtered_tidal_gauges
 from src.data_loading.adcirc import select_coastal_cells
+from src.constants import KAT_EX_PATH, FIGURE_PATH
 
 
-if __name__ == "__main__":
+def tide_plot(stationid=0):
     # python src/plot/tides.py
     tds = filtered_tidal_gauges()
     print(tds)
-    psc = tds.isel(stationid=0)
+    psc = tds.isel(stationid=stationid)
     print((float(psc.lon.values), float(psc.lat.values)))
 
     lons, lats, heights = select_coastal_cells(
         float(psc.lon.values), float(psc.lat.values)
     )
-    start = datetime.datetime(year=2005, month=8, day=19, hour=7)
-    time_step = datetime.timedelta(hours=1.33)
+    start = datetime.datetime(year=2005, month=8, day=19, hour=5)
+    time_step = datetime.timedelta(hours=1, minutes=20)
+    # start = datetime.datetime(year=2005, month=8, day=21, hour=18)
+    # time_step = datetime.timedelta(hours=1)
+    # time_step = datetime.timedelta(hours=1, minutes=15)
+    # start = datetime.datetime(year=2005, month=8, day=20, hour=18)
 
     print(heights.shape)
     ds = xr.Dataset(
@@ -30,8 +36,14 @@ if __name__ == "__main__":
         ),
     )
     print(ds)
-    # plot_defaults()
+    plot_defaults()
 
     psc.water_level.plot()
     ds.height.plot.line(hue="point", alpha=0.5)
-    plt.show()
+    plt.savefig(os.path.join(FIGURE_PATH, "tide_gauge" + str(stationid) + ".png"))
+    plt.clf()
+
+
+if __name__ == "__main__":
+    # python src/plot/tides.py
+    [tide_plot(x) for x in range(4)]
