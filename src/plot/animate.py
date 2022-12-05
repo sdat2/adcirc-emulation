e1f0e761d517e55583a1099c76fce2ce821d6cce
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-12-05 21:17:04 +0000

    animate quiver height

--- src/plot/animate.py
+++ src/plot/animate.py
@@ -6,9 +6,10 @@ import xarray as xr
 import matplotlib
 import matplotlib.pyplot as plt
 import imageio
+from sithom.place import BoundingBox
 from sithom.plot import lim, plot_defaults
 from sithom.xr import plot_units
-from src.constants import FIGURE_PATH, DATA_PATH, NO_BBOX
+from src.constants import FIGURE_PATH, DATA_PATH, NO_BBOX, NEW_ORLEANS
 from src.data_loading.adcirc import timeseries_height_ds, read_windspeeds
 
 
@@ -21,14 +22,13 @@ def animate_height_timeseries(ds: xr.Dataset, output_path: str) -> None:
         output_path (str): output.
     """
     output_path = os.path.join(FIGURE_PATH, output_path)
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
     plot_defaults()
     vmin, vmax = lim(ds.zeta.values, percentile=0, balance=True)
     vmin, vmax = np.min([-vmax, vmin]), np.max([-vmin, vmax])
     levels = np.linspace(vmin, vmax, num=400)
     cbar_levels = np.linspace(vmin, vmax, num=7)
-
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
 
     def plot_part(num: int = 125) -> None:
         plt.tricontourf(
@@ -242,10 +242,104 @@ def plot_quiver_height(path_in: str = "mult1", num: int = 185) -> None:
     plt.clf()
 
 
+def animate_quiver_height(
+    path_in: str = "mult1",
+    output_path: str = "katrina_hit",
+    bbox: BoundingBox = NO_BBOX,
+) -> None:
+    plot_defaults()
+    output_path = os.path.join(FIGURE_PATH, output_path)
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    path_in = os.path.join(DATA_PATH, path_in)
+    ds = timeseries_height_ds(path=path_in, bbox=bbox).sel(
+        time=slice("2005-08-28", "2005-08-30")
+    )
+    dsw = read_windspeeds(os.path.join(path_in, "fort.222"))
+    print(ds)
+    vmin, vmax = lim(ds.zeta.values, percentile=0, balance=True)
+    vmin, vmax = np.min([-vmax, vmin]), np.max([-vmin, vmax])
+    levels = np.linspace(vmin, vmax, num=400)
+    cbar_levels = np.linspace(vmin, vmax, num=5)
+
+    def plot_part(num=185):
+        plt.tricontourf(
+            ds.lon.values,
+            ds.lat.values,
+            ds.mesh.values,
+            ds.zeta.values[num],
+            vmin=vmin,
+            vmax=vmax,
+            levels=levels,
+            cmap="cmo.balance",
+        )
+        ax = plt.gca()
+        cbar = plt.colorbar(label="Sea Surface Height [m]")
+        cbar.set_ticks(cbar_levels)
+        cbar.set_ticklabels(["{:.2f}".format(x) for x in cbar_levels.tolist()])
+        plt.xlabel("Longitude [$^{\circ}$E]")
+        plt.ylabel("Latitude [$^{\circ}$N]")
+        time = ds.isel(time=num).time.values
+        ts = pd.to_datetime(str(time))
+        # plt.savefig(os.path.join(output_path, str(num) + ".png"))
+        # plt.clf()
+        # _, ax = plt.subplots(1, 1)
+        quiver = plot_units(
+            dsw.sel(time=time, method="nearest"), x_dim="lon", y_dim="lat"
+        ).plot.quiver(
+            ax=ax,
+            x="lon",
+            y="lat",
+            u="U10",
+            v="V10",
+            add_guide=False,
+        )
+        # x_pos = 0.65
+        # y_pos = -0.15
+        x_pos = 0.95
+        y_pos = -0.15
+        _ = plt.quiverkey(
+            quiver,
+            # 1.08,
+            x_pos,
+            y_pos,  # 08,
+            40,
+            str(r"$40$ m s$^{-1}$"),  # + "\n"
+            labelpos="E",
+            coordinates="axes"
+            # coordinates="figure"
+            # ,
+        )
+        bbox.ax_lim(plt.gca())
+        plt.title(ts.strftime("%Y-%m-%d  %H:%M"))
+        plt.savefig(os.path.join(output_path, str(num) + ".png"))
+        plt.clf()
+
+    for i in range(len(ds.time.values)):
+        plot_part(i)
+
+    fig_paths = [
+        os.path.join(output_path, str(num) + ".png")
+        for num in range(len(ds.time.values))
+    ]
+    ims = [imageio.imread(f) for f in fig_paths]
+    imageio.mimwrite(output_path + ".gif", ims)
+
+
 if __name__ == "__main__":
     plot_defaults()
     # python src/plot/animate.py
     # trim_and_animate("kate_h08", "katrina_hit5")
     # trim_and_animate("katd_h08", "katrina_hit5")
     # trim_and_animate("mult2", "katrina_mult2")
-    plot_quiver_height()
+    # animate_quiver_height(
+    #     path_in="mult1", output_path="katrina_hit_larger", bbox=NEW_ORLEANS.bbox(5)
+    # )
+    bbox = NEW_ORLEANS.bbox(3)
+    bbox.lat = [x - 1 for x in bbox.lat]
+
+    animate_quiver_height(
+        path_in="katd_h08",
+        output_path="katrina_hit_medium_holland",
+        bbox=bbox,
+    )
