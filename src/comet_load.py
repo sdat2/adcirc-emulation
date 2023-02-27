264f4941d6a28f322cee682dbcf0f2d3343074fb
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-02-27 22:19:49 +0000

    comet load

--- src/comet_load.py
+++ src/comet_load.py
@@ -7,19 +7,19 @@ from comet_ml import API
 comet_ml.config.save(api_key="57fHMWwvxUw6bvnjWLvRwSQFp")
 
 comet_api = API()
-workspace = "sdat2/6dactive"
-found = False
-for project in comet_api.get(workspace):
-    if found:
-        break
-    print("    processing project", project, "...")
-    print("        processing experiment", exp.id, end="")
+print(comet_api.get())
+
+
+def loop_through_project():
+    workspace = "sdat2"  # /6dactive"
+    project = "6dactive"
+
     for exp in comet_api.get(workspace, project):
+        print("    processing project", project, "...")
+        print("        processing experiment", exp.id, end="")
         print(".", end="")
-        if exp.get_html() != None:
-            print("\nFound html in %s!" % exp.url)
-            found = True
-            break
+        print(comet_api.get(workspace, project, exp.id))
 
 
+loop_through_project()
 # python src/comet_load.py
