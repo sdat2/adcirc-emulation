9f67d85763513513dbefa45f6d745e8a02272ed5
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-02-27 22:47:11 +0000

    loop through experiment

--- src/comet_load.py
+++ src/comet_load.py
@@ -21,5 +21,22 @@ def loop_through_project():
         print(comet_api.get(workspace, project, exp.id))
 
 
-loop_through_project()
+def loop_through_experiment():
+    workspace = "sdat2"  # /6dactive"
+    project = "6dactive"
+    # experiment = "f5e7f5b6d8c34f9b9c3d7e5a6d2f2c3"
+    experiment = "261f1786b8ab496e90170d593deba88f"
+
+    exp = comet_api.get(workspace, project, experiment)
+    for metric in ["mae", "rmse", "r2", "inum", "anum"]:
+        metrics = exp.get_metrics(metric)
+        print("len(metrics)", len(metrics))
+        print("metrics", metrics)
+        for i in range(len(metrics)):
+            print("metrics[" + str(i) + "]", metrics[i]["metricValue"])
+    # print("metrics[0]", metrics[0]["metricValue"])
+
+
+# loop_through_project()
+loop_through_experiment()
 # python src/comet_load.py
