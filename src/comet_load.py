e52b2d640acc92a24204bb01ed28823a521cc502
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-02-28 11:31:18 +0000

    plot inum metrics

--- src/comet_load.py
+++ src/comet_load.py
@@ -1,10 +1,14 @@
 """
 Comet load.py
 """
+import os
+from typing import List
 import comet_ml
 from comet_ml import API
 import xarray as xr
+import matplotlib.pyplot as plt
 from src.constants import FIGURE_PATH
+from sithom.plot import plot_defaults
 
 comet_ml.config.save(api_key="57fHMWwvxUw6bvnjWLvRwSQFp")
 
@@ -23,11 +27,12 @@ def loop_through_project():
         print(comet_api.get(workspace, project, exp.id))
 
 
-def loop_through_experiment():
+def loop_through_experiment() -> List[xr.Dataset]:
     workspace = "sdat2"  # /6dactive"
     project = "6dactive"
     # experiment = "f5e7f5b6d8c34f9b9c3d7e5a6d2f2c3"
     # experiment = "261f1786b8ab496e90170d593deba88f"
+    ds_list = []
     for exp in comet_api.get(workspace, project):
 
         # exp = comet_api.get(workspace, project, experiment)
@@ -48,9 +53,32 @@ def loop_through_experiment():
             #    print("metrics[" + str(i) + "]", metrics[i]["metricValue"])
         # print("metrics[0]", metrics[0]["metricValue"])
         ds = xr.Dataset(data_vars=metric_d)
-        print(ds)
+        ds_list.append(ds)
 
+    return ds_list
+
+
+def plot_inum_metrics(ds_list: List[xr.Dataset]) -> None:
+    plot_defaults()
+    os.makedirs(os.path.join(FIGURE_PATH, "6dactive"), exist_ok=True)
+    fig, axs = plt.subplots(3, 1, sharex=True)
+    axs[0].set_ylabel("r2")
+    axs[1].set_ylabel("mae")
+    axs[2].set_ylabel("rmse")
+    axs[2].set_xlabel("Number of Latin Hypercube Samples")
+    for ds in ds_list:
+        ds = ds.where(ds.anum == 0, drop=True)
+        axs[0].plot(ds.inum.values, ds.r2.values)
+        # axs[0].plot(ds.r2)
+        axs[1].plot(ds.inum.values, ds.mae)
+        axs[2].plot(ds.inum.values, ds.rmse)
+
+    plt.savefig(os.path.join(FIGURE_PATH, "6dactive", "inum_metrics.png"))
+    plt.clf()
+
+
+plot_inum_metrics(loop_through_experiment())
 
 # loop_through_project()
-loop_through_experiment()
+# loop_through_experiment()
 # python src/comet_load.py
