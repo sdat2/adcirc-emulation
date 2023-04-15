6f50538dc38e10198b6bb83f0c68d54ac0b033f6
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-04-15 16:13:32 +0100

    ok

--- src/comet_load.py
+++ src/comet_load.py
@@ -9,7 +9,7 @@ from comet_ml import API
 import xarray as xr
 import matplotlib.pyplot as plt
 from src.constants import FIGURE_PATH
-from sithom.plot import plot_defaults, label_subplots
+from sithom.plot import plot_defaults, label_subplots, get_dim
 
 comet_ml.config.save(api_key="57fHMWwvxUw6bvnjWLvRwSQFp")
 
@@ -187,7 +187,7 @@ def plot_final_metrics(ds_list: List[xr.Dataset]) -> None:
     plt.ylabel("Fraction of Actively Chosen Points [-]")
     # plt.show()
     plt.clf()
-    fig, axs = plt.subplots(3, 1, sharex=True)
+    fig, axs = plt.subplots(3, 1, sharex=True, figsize=get_dim(ratio=1.5))
     axs[0].set_ylabel("r$^{2}$ [-]")
     axs[1].set_ylabel("MAE [m]")
     axs[2].set_ylabel("RMSE [m]")
@@ -203,8 +203,8 @@ def plot_final_metrics(ds_list: List[xr.Dataset]) -> None:
         axs[0].plot(frac_t, r2_t, label=f"{j}")
         axs[1].plot(frac_t, mae_t)
         axs[2].plot(frac_t, rmse_t)
+    label_subplots(axs)
     plt.xlabel("Fraction of Actively Chosen Points [-]")
-    plt.ylabel("r$^{2}$ [-]")
     axs[0].legend(title="Total Number of Samples [-]")
     plt.savefig(os.path.join(FIGURE_PATH, "6dactive", "frac_r2.png"))
 
